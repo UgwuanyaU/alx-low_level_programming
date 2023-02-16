daffaa203596f7c54c2376eaef80b45cd519788a
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-16 13:04:17 -0800

    Task 8c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {

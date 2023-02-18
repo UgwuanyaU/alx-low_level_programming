0c5a77db22cb0262f0be2973b9c7ea960c237f74
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-18 15:00:17 -0800

    Task 1f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (last_digit > 5)
 	{
-		printf("%s %d is %d and is greater than 5\n", b_itext, n, last_digit);
+		printf("%s %d is %d and is greater than 5\n", b_text, n, last_digit);
 	}
 	else
 	{

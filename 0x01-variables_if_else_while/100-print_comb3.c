10fe98054303656e500915ce069fcfd0c31a7450
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-17 21:50:54 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '0'; hundreds++)
+	{
+		for (tens = '0'; tens <= '0'; tens++)
+		{
+			for (ones = '0'; ones <= '0'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds)) ||
+						(tens > ones) || (hundreds > tens)) /*eliminations repitition*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '0' && hundreds == '/' &&
+								tens == '0')) /*adds commas and spaces*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

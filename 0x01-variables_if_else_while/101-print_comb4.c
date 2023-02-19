7454d99cd108c91d8d70422477dc584fc7e98fff
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-19 07:29:05 -0800

    Task 11b

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,32 +7,29 @@
 
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int n, m, l;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (n = 48; n < 58; n++)
 	{
-		for (tens = '0'; ones <= '9'; tens++)
+		for (m = 49; m < 58; m++)
 		{
-			for (ones = '0'; ones <= '9' ones++)
+			for (l = 50; l < 58; l++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens)))/* eliminates reposition */
-						{
-							putchar(hundreds);
-							putchar(tens);
-							putchar(ones);
-							if (!(ones == '9' && hundreds == '7' &&
-										tens == '8')) /* add commas and spaces */
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
+

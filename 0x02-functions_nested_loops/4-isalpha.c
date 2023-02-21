ae20a92c43d680526842d6877ffd11b8c4181bae
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-21 14:08:24 -0800

    Task 4b

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is letter, 0 otherwise
  */
@@ -10,7 +10,7 @@ int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-	else 
+else
 		return (0);
 	_putchar ('\n');
 }

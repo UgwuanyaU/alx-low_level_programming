e880837f2e15a425e06fef33bec6cd12aa4ed64f
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-22 03:57:03 -0800

    Task 15b

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,28 +7,17 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
+	while (k + j < 4000000)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = K;
-		k = next;
-
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
 	}
-
-	printf("%lu\n", sum);
-
+	printf("%ld\n", sum);
 	return (0);
-
 }
-

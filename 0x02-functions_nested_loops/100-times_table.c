b09c07a2ebb1f74f02584e43297e78df98397897
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-22 05:12:19 -0800

    Task 12f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,8 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');

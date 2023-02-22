759299fc54fadbec2739aa4b4244722659e43d90
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-22 07:18:25 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - A test to print function for possitive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

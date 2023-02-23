5132d39971336439949d905ade7031de47609f78
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-23 09:24:11 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Print - Print 0 to 9 and do not print 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}

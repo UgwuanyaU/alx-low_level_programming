56129e6010fccff213fd170bd084b9b9ec04e738
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-23 08:45:38 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function for numberss that prints the numbers
+ * from 0 t0 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}

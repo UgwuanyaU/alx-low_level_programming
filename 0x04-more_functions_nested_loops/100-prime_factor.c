22806c617acd77e2b2ffc89dd4cea1c7396306f4
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-23 13:05:23 -0800

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (n = 1; n <= square; n++)
+	{
+		if (number % n == 0)
+		{
+			maxf = number / n;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
+

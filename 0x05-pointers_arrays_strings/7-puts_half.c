487b988b43dbdaa8dfbf00e3aef485d03a561bcd
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-02-27 11:05:55 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a srting
+ * @str: The string to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		j = (i - 1) / 2;
+		j += 1;
+		else
+		{
+			j = i / 2;
+		}
+		for (; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
+	}

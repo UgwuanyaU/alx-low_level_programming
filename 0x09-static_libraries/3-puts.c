606d5bbbd13b82b2e035ca3b0e4ee1282819b315
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-03-10 18:57:18 -0800

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - Entry point
+ * @s: character
+ * Return: 0
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, i'm done \"\n\t - Kevin Mitnick");
+	return (0);
+}

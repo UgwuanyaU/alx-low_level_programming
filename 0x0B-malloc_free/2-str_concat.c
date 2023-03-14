469f26f8e00778965d533f00b863cb8e84c4ecb3
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-03-14 12:15:10 -0700

    Task 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,22 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 
 	if (s2 == NULL)
+	{
 		s2 = "";
-
+	}
 		while (s1[i])
+		{
 		i++;
-
+		}
 	while (s2[j])
+	{
 		j++;
-
+	}
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 	if (s == NULL)

2faf5d97add611aa2a4c3d1344acfdcc356ce01d
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-03-14 11:52:02 -0700

    Task 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,34 @@
 char *str_concat(char *s1, char *s2)
 
 {
-	char *conct;
-	int i, ci;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
-	while (s1[i] != '\0')
+		while (s1[i])
 		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
+	while (s2[j])
+		j++;
+	l = i + j;
+	conct = malloc((sizeof(char) * l) + 1);
+	if (s == NULL)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
+	j = 0;
+	while (k < l)
 	{
-		conct[i] = s1[i];
-		i++;
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
 	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	s[k] = '\0';
+	return (s);
 }

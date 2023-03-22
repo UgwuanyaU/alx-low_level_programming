4add90c9b6d8fee5e084b68ddb5954437e181142
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-03-22 11:29:17 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
+	{
 		return (-1);
+	}
 
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
+			{
 				return (i);
+			}
 		}
 		return (-1);
-	}
+}

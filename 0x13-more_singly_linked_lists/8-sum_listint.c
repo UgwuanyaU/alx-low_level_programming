a66ade2c39bd76e896800b454d8025e731e4eb80
Ugwuanya Daniel Ugochukwu <ugwuanyadaniel@gmail.com>
2023-04-04 12:36:22 -0700

    Tasks 8&9

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,7 +10,7 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	listint_t *temp = head;
-	
+
 	while (temp)
 	{
 		sum += temp->n;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	
+
 	if (idx == 0)
 	{
 		new->next = *head;
